cec03d195356f86b7010313dc7070b47a425a1c3
Victoria Gonzalez <victoria.elicode@gmail.com>
2022-03-01 21:43:03 -0300

    pruebas

---
+++ ventaherramienta/src/Components/Cart.jsx
@@ -0,0 +1,14 @@
+import React, { Component} from "react";
+
+class Cart extends Component {
+    render() {
+        return (
+            <div>
+                Nuevo
+            </div>
+        );
+
+    }
+}
+
+export default Cart
