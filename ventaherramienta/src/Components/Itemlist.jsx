8ec34669cb6bd0191771d5864378ad1fc10759ae
Victoria Gonzalez <victoria.elicode@gmail.com>
2022-03-13 18:11:09 -0300

    primeros elementos

--- ventaherramienta/src/Components/Itemlist.jsx
+++ ventaherramienta/src/Components/Itemlist.jsx
@@ -10,4 +10,40 @@ const Itemlist = () =>{
     );
 }
 
+function imprimirProductos (){
+    return new Promise((resolve,reject)=> {
+        setTimeout(() => {
+            let productos = [
+                {nombre:"Martillo", tipo : "Manual"},
+                {nombre:"Destornillador", tipo : "Manual"},
+                {nombre:"Sierra", tipo : "Eléctrica"}
+            ] 
+            if(productos.length===0){
+                reject("No hay productos disponibles")
+            }else{
+                resolve(productos)
+            }
+
+        }, 2000);
+    })
+}
+
+imprimirProductos()
+.then((productos)=> {
+    console.log(productos)
+    return productos    
+})
+.then((productos) => {
+    if(productos.length === 1){
+       /*  console.log("Solo hay dos productos") */
+       throw new Error("Error hay solo un producto")
+    }else{
+        console.log(productos)
+    }
+
+})
+.catch((err)=>{
+    console.log("Error con productos" + err)
+})
+
 export default Itemlist
